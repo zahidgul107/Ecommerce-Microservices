d7564bc94ac0b7e5fd5519fca1154350fa2b391a
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-03-26 15:31:34 +0530

    added api for product service

--- OrderService/src/main/java/com/order_service/service/OrderService.java
+++ OrderService/src/main/java/com/order_service/service/OrderService.java
@@ -1,5 +1,9 @@
 package com.order_service.service;
 
+import com.order_service.model.OrderRequest;
+
 public interface OrderService {
+
+	long placeOrder(OrderRequest orderRequest);
 
 }
--- OrderService/src/main/java/com/order_service/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/order_service/service/OrderServiceImpl.java
@@ -1,5 +1,42 @@
 package com.order_service.service;
 
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.order_service.entity.Order;
+import com.order_service.model.OrderRequest;
+import com.order_service.repository.OrderRepository;
+
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
 public class OrderServiceImpl implements OrderService {
+	
+	@Autowired
+	OrderRepository orderRepo;
+	
+	
+	@Override
+	public long placeOrder(OrderRequest orderRequest) {
+	
+	
+		
+		log.info("Placing order request: {}", orderRequest);
+		
+		Order order = Order.builder()
+				.amount(orderRequest.getTotalAmount())
+				.orderStatus("CREATED")
+				.productId(orderRequest.getProductId())
+				.orderDate(Instant.now())
+				.quantity(orderRequest.getQuantity())
+				.build();
+		
+		order = orderRepo.save(order);
+		log.info("Order placed successfully with Order Id: {}", order.getId());
+		return order.getId();
+	}
 
 }
