21d73cbebbb8e662ea22af4e011be42298720ed6
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-03-31 22:51:36 +0530

    get product details with order

--- OrderService/src/main/java/com/order_service/OrderServiceApplication.java
+++ OrderService/src/main/java/com/order_service/OrderServiceApplication.java
@@ -2,7 +2,10 @@ package com.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,6 +13,14 @@ public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
+		
+		
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
