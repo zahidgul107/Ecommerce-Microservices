31f989e566928c9eae37ef54ae6661082a5aa343
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-03-31 20:09:02 +0530

    get order

--- OrderService/src/main/java/com/order_service/controller/OrderController.java
+++ OrderService/src/main/java/com/order_service/controller/OrderController.java
@@ -3,12 +3,15 @@ package com.order_service.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.order_service.model.OrderRequest;
+import com.order_service.model.OrderResponse;
 import com.order_service.service.OrderService;
 
 import lombok.extern.log4j.Log4j2;
@@ -27,6 +30,12 @@ public class OrderController {
 		log.info("Order Id: {}", orderId);
 		
 		return new ResponseEntity<>(orderId, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{orderId}")
+	public ResponseEntity<OrderResponse> getOrderDetails(@PathVariable long orderId) {
+		OrderResponse orderResponse = orderSer.getOrderDetails(orderId);
+		return new ResponseEntity<>(orderResponse, HttpStatus.OK);
 	}
 
 }
